30bcd40a992aba6c7a05e3b44dd830ebe995fd02
aydiv <aydiv.c@gmail.com>
2012-11-27 18:59:54 +0530

    Upgrading to refactored SDK

--- README.md
+++ README.md
@@ -2,12 +2,23 @@
 PayPal PHP ButtonManager SDK
 ===============================
 
+
+This is a "Work in progress" SDK. If you are looking for a ButtonManager SDK, please see 
+
+https://www.x.com/developers/paypal/documentation-tools/paypal-sdk-index#buttonmanagernew
+
+  OR 
+
+https://github.com/paypal/SDKs/tree/master/ButtonManager/buttonmanager-php-sdk
+
+
+
 Prerequisites
 -------------
 
 PayPal's PHP ButtonManager SDK requires 
 
- * PHP 5.2 and above with curl extension enabled
+   * PHP 5.2 and above with curl/openssl extensions enabled
   
 
 Using the SDK
@@ -15,33 +26,47 @@ Using the SDK
 
 To use the SDK, 
 
-* Copy the config and lib folders into your project.
-* Make sure that the lib folder in your project is available in PHP's include path
-* Include the services\PayPalAPIInterfaceService\PayPalAPIInterfaceServiceService.php file in your code.
-* Create a service wrapper object
-* Create a request object as per your project's needs. All the API request and response classes are available in services\PayPalAPIInterfaceService\PayPalAPIInterfaceServiceService.php
-* Invoke the appropriate method on the request object.
+   * Copy the config and lib folders into your project. Modify the config file sdk_config.ini to suit your needs.
+   * Make sure that the lib folder in your project is available in PHP's include path
+   * Include the services\PayPalAPIInterfaceService\PayPalAPIInterfaceServiceService.php 
+     file in your code.
+   * Instantiate a service wrapper object.
+   * Instantiate a request object as per your project's needs. All the API request and response 
+     classes are available in services\PayPalAPIInterfaceService\PayPalAPIInterfaceServiceService.php
+   * Invoke the appropriate method on the service object.
 
 
 For example,
 
 	require_once('services/PayPalAPIInterfaceService/PayPalAPIInterfaceServiceService.php');	require_once('PPLoggingManager.php');
 
-    $buttonSearchRequest = new BMButtonSearchRequestType();
-	$buttonSearchRequest->Version = 86.0;
-	
 	$buttonSearchReq = new BMButtonSearchReq();
-	$buttonSearchReq->BMButtonSearchRequest = $buttonSearchRequest;
+	$buttonSearchReq->BMButtonSearchRequest = new BMButtonSearchRequestType();
 	......
 
 	$paypalService = new PayPalAPIInterfaceServiceService();
 	$buttonSearchResponse = $paypalService->BMButtonSearch($buttonSearchReq);
 	
-	$ack = strtoupper($buttonSearchResponse->Ack);
- 
+	$ack = strtoupper($buttonSearchResponse->Ack); 
 	if($ack == 'SUCCESS') {
 		// Success
 	}
+
+
+The SDK provides multiple ways to authenticate your API call.
+
+	$paypalService = new PayPalAPIInterfaceServiceService();
+	
+	// Use the default account (the first account) configured in sdk_config.ini
+	$response = $paypalService->BMButtonSearch($buttonSearchReq);	
+
+	// Use a specific account configured in sdk_config.inig
+	$response = $paypalService->BMButtonSearch($buttonSearchReq, 'jb-us-seller_api1.paypal.com');	
+	 
+	// Pass in a dynamically created API credential object
+    $cred = new PPCertificateCredential("username", "password", "path-to-pem-file");
+    $cred->setThirdPartyAuthorization(new PPTokenAuthorization("accessToken", "tokenSecret"));
+	$response = $paypalService->BMButtonSearch($buttonSearchReq, $cred);	
   
  
 
@@ -50,8 +75,21 @@ SDK Configuration
 
 replace the API credential in config/sdk_config.ini . You can use the configuration file to configure
 
- * (Multiple) API account credentials.
- * Service endpoint and other HTTP connection parameters 
-
+   * (Multiple) API account credentials.
+   * Service endpoint and other HTTP connection parameters 
+   * Logging
 
 Please refer to the sample config file provided with this bundle.
+
+
+
+Getting help
+------------
+
+If you need help using the SDK, a new feature that you need or have a issue to report, please visit
+
+   https://www.x.com/developers/paypal/forums/button-manager-api
+   
+     OR
+   
+   https://github.com/paypal/buttonmanager-php-sdk/issues 
--- lib/services/PayPalAPIInterfaceService/PayPalAPIInterfaceServiceService.php
+++ lib/services/PayPalAPIInterfaceService/PayPalAPIInterfaceServiceService.php
@@ -1,8 +1,6 @@
 <?php 
-require_once('PPBaseService.php');
-require_once('PayPalAPIInterfaceService.php');
-require_once('PPUtils.php');
-
+require_once 'PPBaseService.php';
+require_once 'PayPalAPIInterfaceService.php';
 
 /**
  * AUTO GENERATED code for PayPalAPIInterfaceService
@@ -15,8 +13,14 @@ class PayPalAPIInterfaceServiceService extends PPBaseService {
 	// Service Name
 	private static $SERVICE_NAME = "PayPalAPIInterfaceService";
 
+    // SDK Name
+	private static $SDK_NAME = "sdkname";
+	
+	// SDK Version
+	private static $SDK_VERSION = "sdkversion";
+
 	public function __construct() {
-		parent::__construct('PayPalAPIInterfaceService');
+		parent::__construct(self::$SERVICE_NAME, 'SOAP', array('PPMerchantServiceHandler'));
 	}
 
 	private function setStandardParams(AbstractRequestType $request) {
@@ -28,13 +32,16 @@ class PayPalAPIInterfaceServiceService extends PPBaseService {
 	/**
 	 * Service Call: BMCreateButton
 	 * @param BMCreateButtonReq $bMCreateButtonReq
+	 * @param mixed $apiCredential - Optional API credential - can either be
+	 * 		a username configured in sdk_config.ini or a ICredential object
+	 *      created dynamically 		
 	 * @return BMCreateButtonResponseType
 	 * @throws APIException
 	 */
-	public function BMCreateButton($bMCreateButtonReq, $apiUsername = NULL) {
+	public function BMCreateButton($bMCreateButtonReq, $apiCredential = NULL) {
 		$this->setStandardParams($bMCreateButtonReq->BMCreateButtonRequest);
 		$ret = new BMCreateButtonResponseType();
-		$resp = $this->call("BMCreateButton", $bMCreateButtonReq, $apiUsername);
+		$resp = $this->call("BMCreateButton", $bMCreateButtonReq, $apiCredential);
 		$ret->init(PPUtils::xmlToArray($resp));
 		return $ret;
 	}
@@ -43,13 +50,16 @@ class PayPalAPIInterfaceServiceService extends PPBaseService {
 	/**
 	 * Service Call: BMUpdateButton
 	 * @param BMUpdateButtonReq $bMUpdateButtonReq
+	 * @param mixed $apiCredential - Optional API credential - can either be
+	 * 		a username configured in sdk_config.ini or a ICredential object
+	 *      created dynamically 		
 	 * @return BMUpdateButtonResponseType
 	 * @throws APIException
 	 */
-	public function BMUpdateButton($bMUpdateButtonReq, $apiUsername = NULL) {
+	public function BMUpdateButton($bMUpdateButtonReq, $apiCredential = NULL) {
 		$this->setStandardParams($bMUpdateButtonReq->BMUpdateButtonRequest);
 		$ret = new BMUpdateButtonResponseType();
-		$resp = $this->call("BMUpdateButton", $bMUpdateButtonReq, $apiUsername);
+		$resp = $this->call("BMUpdateButton", $bMUpdateButtonReq, $apiCredential);
 		$ret->init(PPUtils::xmlToArray($resp));
 		return $ret;
 	}
@@ -58,13 +68,16 @@ class PayPalAPIInterfaceServiceService extends PPBaseService {
 	/**
 	 * Service Call: BMManageButtonStatus
 	 * @param BMManageButtonStatusReq $bMManageButtonStatusReq
+	 * @param mixed $apiCredential - Optional API credential - can either be
+	 * 		a username configured in sdk_config.ini or a ICredential object
+	 *      created dynamically 		
 	 * @return BMManageButtonStatusResponseType
 	 * @throws APIException
 	 */
-	public function BMManageButtonStatus($bMManageButtonStatusReq, $apiUsername = NULL) {
+	public function BMManageButtonStatus($bMManageButtonStatusReq, $apiCredential = NULL) {
 		$this->setStandardParams($bMManageButtonStatusReq->BMManageButtonStatusRequest);
 		$ret = new BMManageButtonStatusResponseType();
-		$resp = $this->call("BMManageButtonStatus", $bMManageButtonStatusReq, $apiUsername);
+		$resp = $this->call("BMManageButtonStatus", $bMManageButtonStatusReq, $apiCredential);
 		$ret->init(PPUtils::xmlToArray($resp));
 		return $ret;
 	}
@@ -73,13 +86,16 @@ class PayPalAPIInterfaceServiceService extends PPBaseService {
 	/**
 	 * Service Call: BMGetButtonDetails
 	 * @param BMGetButtonDetailsReq $bMGetButtonDetailsReq
+	 * @param mixed $apiCredential - Optional API credential - can either be
+	 * 		a username configured in sdk_config.ini or a ICredential object
+	 *      created dynamically 		
 	 * @return BMGetButtonDetailsResponseType
 	 * @throws APIException
 	 */
-	public function BMGetButtonDetails($bMGetButtonDetailsReq, $apiUsername = NULL) {
+	public function BMGetButtonDetails($bMGetButtonDetailsReq, $apiCredential = NULL) {
 		$this->setStandardParams($bMGetButtonDetailsReq->BMGetButtonDetailsRequest);
 		$ret = new BMGetButtonDetailsResponseType();
-		$resp = $this->call("BMGetButtonDetails", $bMGetButtonDetailsReq, $apiUsername);
+		$resp = $this->call("BMGetButtonDetails", $bMGetButtonDetailsReq, $apiCredential);
 		$ret->init(PPUtils::xmlToArray($resp));
 		return $ret;
 	}
@@ -88,13 +104,16 @@ class PayPalAPIInterfaceServiceService extends PPBaseService {
 	/**
 	 * Service Call: BMSetInventory
 	 * @param BMSetInventoryReq $bMSetInventoryReq
+	 * @param mixed $apiCredential - Optional API credential - can either be
+	 * 		a username configured in sdk_config.ini or a ICredential object
+	 *      created dynamically 		
 	 * @return BMSetInventoryResponseType
 	 * @throws APIException
 	 */
-	public function BMSetInventory($bMSetInventoryReq, $apiUsername = NULL) {
+	public function BMSetInventory($bMSetInventoryReq, $apiCredential = NULL) {
 		$this->setStandardParams($bMSetInventoryReq->BMSetInventoryRequest);
 		$ret = new BMSetInventoryResponseType();
-		$resp = $this->call("BMSetInventory", $bMSetInventoryReq, $apiUsername);
+		$resp = $this->call("BMSetInventory", $bMSetInventoryReq, $apiCredential);
 		$ret->init(PPUtils::xmlToArray($resp));
 		return $ret;
 	}
@@ -103,13 +122,16 @@ class PayPalAPIInterfaceServiceService extends PPBaseService {
 	/**
 	 * Service Call: BMGetInventory
 	 * @param BMGetInventoryReq $bMGetInventoryReq
+	 * @param mixed $apiCredential - Optional API credential - can either be
+	 * 		a username configured in sdk_config.ini or a ICredential object
+	 *      created dynamically 		
 	 * @return BMGetInventoryResponseType
 	 * @throws APIException
 	 */
-	public function BMGetInventory($bMGetInventoryReq, $apiUsername = NULL) {
+	public function BMGetInventory($bMGetInventoryReq, $apiCredential = NULL) {
 		$this->setStandardParams($bMGetInventoryReq->BMGetInventoryRequest);
 		$ret = new BMGetInventoryResponseType();
-		$resp = $this->call("BMGetInventory", $bMGetInventoryReq, $apiUsername);
+		$resp = $this->call("BMGetInventory", $bMGetInventoryReq, $apiCredential);
 		$ret->init(PPUtils::xmlToArray($resp));
 		return $ret;
 	}
@@ -118,13 +140,16 @@ class PayPalAPIInterfaceServiceService extends PPBaseService {
 	/**
 	 * Service Call: BMButtonSearch
 	 * @param BMButtonSearchReq $bMButtonSearchReq
+	 * @param mixed $apiCredential - Optional API credential - can either be
+	 * 		a username configured in sdk_config.ini or a ICredential object
+	 *      created dynamically 		
 	 * @return BMButtonSearchResponseType
 	 * @throws APIException
 	 */
-	public function BMButtonSearch($bMButtonSearchReq, $apiUsername = NULL) {
+	public function BMButtonSearch($bMButtonSearchReq, $apiCredential = NULL) {
 		$this->setStandardParams($bMButtonSearchReq->BMButtonSearchRequest);
 		$ret = new BMButtonSearchResponseType();
-		$resp = $this->call("BMButtonSearch", $bMButtonSearchReq, $apiUsername);
+		$resp = $this->call("BMButtonSearch", $bMButtonSearchReq, $apiCredential);
 		$ret->init(PPUtils::xmlToArray($resp));
 		return $ret;
 	}
